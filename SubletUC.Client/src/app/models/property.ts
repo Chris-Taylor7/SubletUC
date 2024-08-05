074a1747e5393c7aa94463136367f77f00d88614
Chris Taylor <chtaylor270@gmail.com>
2024-08-05 16:30:24 -0400

    create property object

---
+++ SubletUC.Client/src/app/models/property.ts
@@ -0,0 +1,18 @@
+export class Property {
+    address?: string;
+    rent?: number;
+    utilities_included?: boolean;
+    average_monthly_utilities?: number;
+    number_of_bathrooms?: number;
+    number_of_roomates?: number;
+    males?: number;
+    females?: number;
+    other_gender?: number;
+    description?: string;
+    availability?: string;
+    pets?: string;
+    washer_dryer?: boolean;
+    driveway_parking?: boolean;
+    street_parking?: boolean;
+    pictures?: File;
+}
